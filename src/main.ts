ed4af074fcb5524c5cc1f7ca8dbcaec886e3461a
Aki <62400162+AkimeAki@users.noreply.github.com>
2023-09-17 13:20:05 +0900

    末尾スラッシュは404

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,20 @@ const app: express.Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((_req: express.Request, res: express.Response, next: express.NextFunction) => {
+app.use((_req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "*");
 	res.header("Access-Control-Allow-Headers", "*");
 	next();
+});
+
+// 末尾スラッシュは404
+app.use((req, res, next) => {
+	if (req.path.slice(-1) === "/" && req.path.length > 1) {
+		res.status(404).json({ message: "404" });
+	} else {
+		next();
+	}
 });
 
 app.use(router);
